004549edb97dc5e3367110fbe6d43b98e56c7218
Lauriel Mesquita <laurielmesquita@me.com>
2020-06-07 20:39:13 -0300

    :art: The page structure is complete

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,8 +1,121 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { View, Text, Image, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
 
 const Detail = () => {
-  return <View />
+  const navigation = useNavigation()
+
+  function handleNavigateBack () {
+    navigation.goBack()
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Image style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1578916171728-46686eac8d58?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80' }} />
+        <Text style={styles.pointName}>Marcadinho da Reciclagem</Text>
+        <Text style={styles.pointItems}>Lâmpadas, Óleo de cozinha, Casco de cerveja</Text>
+
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>Teresna - PI</Text>
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <FontAwesome name="whatsapp" color="#FFF" size={20} />
+          <Text style={styles.buttonText}>WhatsApp</Text>
+        </RectButton>
+        
+        <RectButton style={styles.button} onPress={() => {}}>
+          <Icon name="mail" color="#FFF" size={20} />
+          <Text style={styles.buttonText}>Email</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    paddingTop: 20,
+  },
+
+  pointImage: {
+    width: '100%',
+    height: 160,
+    resizeMode: 'cover',
+    borderRadius: 10,
+    marginTop: 32,
+  },
+
+  pointName: {
+    color: '#322153',
+    fontSize: 28,
+    fontFamily: 'Ubuntu_700Bold',
+    marginTop: 24,
+  },
+
+  pointItems: {
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 16,
+    lineHeight: 24,
+    marginTop: 8,
+    color: '#6C6C80'
+  },
+
+  address: {
+    marginTop: 32,
+  },
+  
+  addressTitle: {
+    color: '#322153',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  },
+
+  addressContent: {
+    fontFamily: 'Roboto_400Regular',
+    lineHeight: 24,
+    marginTop: 8,
+    color: '#6C6C80'
+  },
+
+  footer: {
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderColor: '#999',
+    paddingVertical: 20,
+    paddingHorizontal: 32,
+    paddingBottom: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  
+  button: {
+    width: '48%',
+    backgroundColor: '#34CB79',
+    borderRadius: 10,
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    marginLeft: 8,
+    color: '#FFF',
+    fontSize: 16,
+    fontFamily: 'Roboto_500Medium',
+  },
+});
 
 export default Detail
